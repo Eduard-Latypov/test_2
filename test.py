3f4cd8c73f65f7ec03f22da7711768ab8c18b45d
Eduard Latypov <LatypovEdik1988@yandex.ru>
2023-03-28 18:58:31 +0300

    123

--- test.py
+++ test.py
@@ -1,31 +1,24 @@
-def its_magic():
-    n, k = map(int, input().split())
-    s = input()
-    p = list(map(int, input().split()))
-    d = list(map(int, input().split()))
-    chars = 'abcdefghijklmnopqrstuvwxyz'
-    chrs_dct = {chars[i]: i for i in range(len(chars))}
-    power = 0
-    for i in range(n):
-        my_set = set()
-        idx = p[i]-1
-        chrs_count = {}
-        for j in range(k):
-            chrs_count[s[idx]] = chrs_count.get(s[idx], 0) + 1
-            if chrs_count[s[idx]] == 1:
-                my_set.add(s[idx])
-            else:
-                if d[idx] != 0:
-                    my_set.add(chars[(chrs_dct[s[idx]] +
-                                      (chrs_count[s[idx]] - 1) * d[idx]) % 26])
-                else:
-                    chrs_count[s[idx]] += 1
-            if len(my_set) == 26:
-                power += len(my_set) * (k - j)
+def outlet():
+    n = int(input())
+    city_income = map(int, input().split())
+    city_education = map(int, input().split())
+    city_citizenship = map(int, input().split())
+    q = int(input())
+    income = map(int, input().split())
+    education = map(int, input().split())
+    citizenship = map(int, input().split())
+    city_dct = {indx: {'city_income': tpl[0], 'city_education': tpl[1], 'city_citizenship': tpl[2]}
+                for indx, tpl in enumerate(zip(city_income, city_education, city_citizenship), 1)}
+    for indx, tpl in enumerate(zip(income, education, citizenship), 1):
+        for key in city_dct:
+            if tpl[2] == key and city_dct[key]['city_citizenship'] == 1:
+                print(key, end=' ')
                 break
-            power += len(my_set)
-            idx = p[idx]-1
-    return power
-
-
-print(its_magic())
+            elif tpl[1] == 1 and city_dct[key]['city_income'] <= tpl[0]:
+                print(key, end=' ')
+                break
+            elif tpl[1] == 0 and city_dct[key]['city_education'] == 0 and city_dct[key]['city_income'] <= tpl[0]:
+                print(key, end=' ')
+                break
+        else:
+            print(0, end=' ')
